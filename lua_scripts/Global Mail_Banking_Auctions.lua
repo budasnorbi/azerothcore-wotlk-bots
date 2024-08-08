8b17c0f2cbb7093b5f90e8d33adee5fdcf92ccf5
Budás Norbert <budasnorbert@gmail.com>
2024-08-08 08:15:01 +0200

    fix cant speak

--- lua_scripts/Global Mail_Banking_Auctions.lua
+++ lua_scripts/Global Mail_Banking_Auctions.lua
@@ -2,7 +2,7 @@
 --  While in-game, just pre-pend any of the commands below with "!" or "."  For example, ".bank" or "!mail".
 --  You can also type "#commands" or "$commands" to display the list of available commands.
 --
---  Hosted by Aldori15 on Github: https://github.com/Aldori15/global-mail_banking_auctions 
+--  Hosted by Aldori15 on Github: https://github.com/Aldori15/global-mail_banking_auctions
 -- ----------------------------------------------------------------------------------------------------------
 
 -- Table to store list of possible commands
@@ -11,19 +11,6 @@ local commands = {
     mail = { "mail", "mailbox" },  -- Chat variations for opening the mailbox
     auction = { "auctions", "ah", "auctionhouse" },  -- Chat variations for opening the auction house
 }
-
-local function DisplayChatCommandList(event, unit, message, type, language)
-    -- Convert message to lowercase for case-insensitive comparison
-    local normalizedMessage = string.lower(message)
-
-	if(normalizedMessage == "#commands") or (normalizedMessage == "$commands") then
-	    unit:SendAreaTriggerMessage("|cFF00B0E8.mail  -  opens the global Mailbox|r")
-        unit:SendAreaTriggerMessage("|cFF00B0E8.bank  -  opens the global Bank|r")
-	    unit:SendAreaTriggerMessage("|cFF00B0E8.auctions  -  opens the global Auction House|r") 
-	end
-    -- Return false to prevent further processing
-	return false
-end
 
 local function HandleChatCommand(event, player, message, type, language)
     -- Convert message to lowercase for case-insensitive comparison
@@ -46,5 +33,5 @@ local function HandleChatCommand(event, player, message, type, language)
     end
 end
 
-RegisterPlayerEvent(18, DisplayChatCommandList)
+
 RegisterPlayerEvent(42, HandleChatCommand)
