37f104c043025fde2ecf57a596dd7d6c29d7236a
Budás Norbert <budasnorbert@gmail.com>
2024-08-27 23:54:59 +0200

    prevent bots selling junk on loot

--- modules/mod-junk-to-gold/src/mod_junk_to_gold.cpp
+++ modules/mod-junk-to-gold/src/mod_junk_to_gold.cpp
@@ -9,7 +9,7 @@ public:
 
     void OnLootItem(Player* player, Item* item, uint32 count, ObjectGuid /*lootguid*/) override
     {
-        if (!item || !item->GetTemplate())
+        if (!item || !item->GetTemplate() || player->GetSession()->IsBot())
         {
             return;
         }
